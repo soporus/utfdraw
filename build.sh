49096b46e54a49bd9256013591e66a47789a0d27
soporus <soporus@sopor.us>
2025-09-09 02:06:49 -0500

    build.sh and CMakeLists updates
    build.sh:   clean, rebuild cache, then compile
    CMakeLists: added additional flags

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cmake . && cmake --build .
+rm -rf CMakeFiles && rm CMakeCache.txt && rm Makefile \
+&& cmake . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+&& cmake --build . --clean-first --verbose 
